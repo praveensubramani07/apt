56839a24b2e4abfcae41b59777de58323670d9a2
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-12-09 12:11:47 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,13 @@ export default function Footer() {
   return (
     <div className="footer-basic">
       <footer>
-        <div className="social">
-          <i className="icon ion-social-instagram"></i>
-          <i className="icon ion-social-snapchat"></i>
-          <i className="icon ion-social-twitter"></i>
-         <i className="icon ion-social-facebook"></i>
-        </div>
+        
         <ul className="list-inline">
         <Link to={'./about'}>
           <li className="list-inline-item">Home</li>
           </Link>
         </ul>
-        <p className="copyright">Company Name © 2018</p>
+        <p className="copyright">AptMen © </p>
       </footer>
     </div>
   )
